94b2b5440b20262045625b853af264ecfe70ba23
Wouter <wouter97@planet.nl>
2018-06-07 13:41:39 +0200

    Cleanup

---
+++ router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.options(/\/api*/, (req,res) => {
+    res.status(204).send().end();
+});
+
+router.use((error,req,res,next) => {
+	res.status(error.status || 500).send({
+        message: error.message,
+        code: error.code,
+        name: error.name,
+        status: error.status
+	}).end();
+});
+
+router.get('*', (req, res) => {
+    res.status(404).send({
+        message: '404 not found'
+    }).end();
+});
+
+module.exports = router;
