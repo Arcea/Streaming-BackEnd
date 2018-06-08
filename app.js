6ff4164864c5b0b2508ac8d58171bf3eed18873c
Thijmen Boot <thijmenboot@outlook.com>
2018-06-08 10:41:58 +0200

    Added mongoose

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var app = express();
 let tokenauth = require('./middleware/tokenauth.js');
 let logger = require('./middleware/logger');
 
+const mongoose = require('mongoose');
+
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
