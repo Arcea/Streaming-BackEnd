4f7b43474c066d1fc25da636dc00e6af11ca0e6a
Arcea <rowinvanblokland@hotmail.nl>
2018-06-08 13:58:46 +0200

    Few changes

--- middleware/tokenauth.js
+++ middleware/tokenauth.js
@@ -4,11 +4,13 @@ let tokenModel = require("./../models/Tokens");
 
 function tokenauth(req, res, next) {
   if (req.url == "/login") {
-    let token = tokenHelper.genToken();
-    // Write token to db
-    res.status(200).json({
-      token: token
+    let token = tokenHelper();
+    token.save(function(err, newToken) {
+      if (err) return console.log(err);
     });
+    // Write token to db
+    res.setHeader("token", token);
+    res.status(200).end();
   } else if (req.headers.token == "" || req.headers.token == undefined) {
     res.status(errors[1401].header).json(errors[1401]);
   } else {
@@ -16,10 +18,10 @@ function tokenauth(req, res, next) {
     // check if token is valid according to signed public key.
     // Delete token from db
     let newToken = tokenHelper.genToken();
-    newToken.save(function (err, newToken){
-      if(err) return console.log(err);
+    newToken.save(function(err, newToken) {
+      if (err) return console.log(err);
     });
-    
+
     res.setHeader("token", newToken);
     next();
   }
