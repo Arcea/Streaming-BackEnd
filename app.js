167c57e1acdc76cbe5d397965fa73b3caf2a220e
Wouter <wouter97@planet.nl>
2018-06-11 14:47:25 +0200

    Added extra header settings

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Expose-Headers: Token');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, Token');
     next();
 });
 
