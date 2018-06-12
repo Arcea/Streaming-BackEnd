8497b75a5073f8f3934f3dda892281c0ab9a2f46
Arcea <rowinvanblokland@hotmail.nl>
2018-06-12 11:35:37 +0200

    Corrected Maarten mistake

--- controllers/StreamController.js
+++ controllers/StreamController.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-const Streams = require("../models/streams");
+const Streams = require("./../models/streams");
 
 module.exports = {
-    getStreams(req, res, next){
-			Streams.find()
-				.then((streams, err) => {
-					if (err) throw err;
-					res.status(200).json(streams)
-				})
-				.catch((err) => {
-					next(err)
-				})
-			}
+	getStreams(req, res, next) {
+		Streams.find()
+			.then((streams, err) => {
+				if (err) throw err;
+				res.status(200).json(streams)
+			})
+			.catch((err) => {
+				next(err)
+			})
+	}
 }
