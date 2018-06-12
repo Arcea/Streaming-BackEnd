b659a6eca2ee74bd41cd445242c9dc4ce85cdc11
Wouter <wouter97@planet.nl>
2018-06-12 10:32:30 +0200

    Changed error code baseline

--- libs/errorcodes.js
+++ libs/errorcodes.js
@@ -2,17 +2,17 @@ var errors = {
   1301: {
     errorCode: 1301,
     message: "error",
-    header: 400
+    header: 200
   },
   1401: {
     errorCode: 1401,
     message: "Invalid token",
-    header: 403
+    header: 200
   },
   1402: {
     errorCode: 1402,
     message: "Authentication failed",
-    header: 400
+    header: 200
   }
 };
 
