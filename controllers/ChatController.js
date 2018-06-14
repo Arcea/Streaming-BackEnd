9269f361f737ad3597abd853858151388e54137c
Wouter <wouter97@planet.nl>
2018-06-14 12:55:59 +0200

    ChatController

--- controllers/ChatController.js
+++ controllers/ChatController.js
@@ -1,15 +1,16 @@
 var db = require('../config/db');
 let chatModel = require("./../models/Chats");
 let userModel = require("./../models/Users");
-
+let io = require("./../app").io;
+ 
 module.exports = {
     Chat(req, res, next) {
-        userModel.findOne({ Name: req.headers.name }, function (err, foundUser) {
+        userModel.findOne({ Name: req.headers.name }, function(err, foundUser) {
             if (err || foundUser == null || foundUser == undefined || foundUser == "") {
-                // handle error properly.
-                return res.status(errors[1403].header).json(errors[1403]);
+              // handle error properly.
+              return res.json(errors[1403]);
             }
-            else {
+            else{
                 console.log(foundUser);
                 console.log(req.body);
                 try {
@@ -18,40 +19,38 @@ module.exports = {
                         Date: Date.now(),
                         Stream: req.params.id,
                         User: foundUser._id
-                    });  
-                } catch (err) {
-                    res.status(errors[1601].header).json(errors[1601]);
-                }
-        
-                chatMessage.save(function(err, newChat) {
-                    if (err){ 
-                        return console.log(err);
-                    } else {
-                        res.status(200).json("Message sent");
-                    }
-                });
+                    });
+            
+                    chatMessage.save(function(err, newChat) {
+                        if (err){ 
+                            return console.log(err);
+                        } else{
+                            res.status(200).json("Message sent");
+                        }
+                    });
+     
+                    io.to(req.params.id).emit(chatMessage);
+                    console.log("Emitted message: " + chatMessage.content);
+                    console.log("Room: " + req.params.id);    
+                } catch (err) { 
+                    res.status(errors[1601].header).json(errors[1601]); 
+                } 
 
-                io.to(req.params.id).emit(chatMessage);
-                console.log("Emitted message: " + chatMessage.content);
-                console.log("Room: " + req.params.id);
             }
         });
     },
     GetStreamChat(req, res, next) {
         chatModel
             .find({ Stream: req.params.id })
-            .populate("User")
             .then((foundChat, err) => {
                 if (err || foundChat === null || foundChat === undefined || foundChat === "") {
-                    if (err) throw err
+                    if(err) throw err
                     return res.json("No chats found");
                 } else {
                     res.status(200).json(foundChat);
                 }
             })
-            .catch(err => {
-                console.log(err)
-            })
-        ;
-    }
-}
+1 file change in working directory
+View change
+commit:9a47cc
+Logging messages
