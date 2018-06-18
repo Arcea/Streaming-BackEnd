043825a034158d0157434bfa2a3335dfaf4393e6
survivorbat <djbatcat@gmail.com>
2018-06-18 17:31:38 +0200

    Added login thing

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const userModel = require('./models/Users')
 //Declare any routes here.
 
 const chatRoutes = require("./routes/chat_routes.js");
@@ -13,13 +14,36 @@ router.use("/streams", streamRoutes);
 router.use("/users", userRoutes);
 
 router.get("/login", (req, res) => {
-  res
-    .status(204)
-    .send()
-    .end();
+          userModel
+            .findOne({ Name: req.headers.name })
+            .then((foundUser, err) => {
+                if (err || foundUser === null || foundUser === undefined || foundUser === "") {
+                    if(err) throw err
+                    res.json("No users found");
+                } else {
+                    res.setHeader("Signature", signData(foundUser))
+                    res.status(200).json(foundUser);
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            });
 });
 router.get('/token', (req, res) => {
-  res.status(204).send().end();
+          userModel
+            .findOne({ Name: req.headers.name })
+            .then((foundUser, err) => {
+                if (err || foundUser === null || foundUser === undefined || foundUser === "") {
+                    if(err) throw err
+                    res.json("No users found");
+                } else {
+                    res.setHeader("Signature", signData(foundUser))
+                    res.status(200).json(foundUser);
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            });
 });
 
 router.use((error, req, res, next) => {
