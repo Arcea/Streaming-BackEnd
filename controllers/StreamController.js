72c32d9d95c400a219ba50d1b80c94a4e3d8199d
thijmenboot <thijmenboot@outlook.com>
2018-06-18 18:50:44 +0200

    Fixed endpoint

--- controllers/StreamController.js
+++ controllers/StreamController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Streams = require("./../models/Streams");
+const Users = require('./../models/Users');
 let errors = require('./../libs/errorcodes');
 const signData = require('../libs/signature').signData
 
@@ -18,12 +19,18 @@ module.exports = {
 			})
 	},
 	GetOne(req, res, next){
-		Streams.findOne({ _id: req.params.id }).select("-__v")
-			.populate("User", "-Streams")
-			.then((stream, err) => {
-				if(err) throw err;
-				res.setHeader("Signature", signData(stream))
-				res.status(200).json(stream)
+		Users.findOne({ name: req.params.name }).select()
+			.then((user, err) => {
+				Streams.findOne({ User: user._id }).select("-__v")
+				.populate("User", "-Streams")
+				.then((stream, err) => {
+					if(err) throw err;
+					res.setHeader("Signature", signData(stream))
+					res.status(200).json(stream)
+				})
+				.catch((err) => {
+					next(err)
+				})
 			})
 			.catch((err) => {
 				next(err)
@@ -40,6 +47,8 @@ module.exports = {
 			.catch((err) => {
 				next(err)
 			})
+		
+		
 	},
 	addStream(req, res, next) {
 		Stream = new Streams(req.body)
