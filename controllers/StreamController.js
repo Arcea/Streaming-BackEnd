8b5424db5422fda007ce45bcddf89acf9d060d0c
Wouter <wouter97@planet.nl>
2018-06-19 13:43:06 +0200

    Captial N

--- controllers/StreamController.js
+++ controllers/StreamController.js
@@ -32,7 +32,7 @@ module.exports = {
 	},
 	GetOneByName(req, res, next){
 		console.log(req.params.name)
-		Users.findOne({ name: req.params.name }).select()
+		Users.findOne({ Name: req.params.name }).select()
 			.then((user, err) => {
 				Streams.findOne({ User: user._id }).select("-__v")
 				.populate("User", "-Streams")
