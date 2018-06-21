2126563d4e63621013bad02e472de2089f9b084d
survivorbat <djbatcat@gmail.com>
2018-06-21 12:16:27 +0200

    now findbyid

--- controllers/StreamController.js
+++ controllers/StreamController.js
@@ -58,13 +58,13 @@ module.exports = {
 			})
 	},
 	Activate(req, res, next) {
-		Streams.findOneAndUpdate({ _id: req.params.streamid }, { Port: req.body.port, Live: true}, function(err, doc){
+		Streams.findByIdAndUpdate(req.params.streamid, { Port: req.body.port, Live: true}, function(err, doc){
 			if(err) return res.send(500, { error: err});
 			return res.send("Saved stream status");
 		});
 	},
 	Deactivate(req, res, next) {
-		Streams.findOneAndUpdate({ _id: req.params.streamid }, { Port: 0, Live: false }, function(err, doc){
+		Streams.findByIdAndUpdate(req.params.streamid, { Port: 0, Live: false }, function(err, doc){
 			if(err) return res.send(500, { error: err});
 			return res.send("Saved stream status");
 		});
